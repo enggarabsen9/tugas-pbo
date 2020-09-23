9e0f5a3eac534f0052e400793a2c9e2243145a6d
enggarabsen9 <tugasgithub123@gmail.com>
2020-09-23 20:51:19 +0700

    tugas

---
+++ src/arsipku2/Biodata.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arsipku2;
+
+/**
+ *
+ * @author kholifah
+ */
+public interface Biodata {
+    
+}
